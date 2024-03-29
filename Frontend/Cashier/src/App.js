9427346d33865f98842cf20870205c62277d3455
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2024-03-29 11:58:14 +0530

    cashier updated.

--- Frontend/Cashier/src/App.js
+++ Frontend/Cashier/src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+import { SearchBar } from './components/SearchBar';
 
-class App extends Component {
-  render() {
+function App() {
   return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm/>
-      </Container>
+    <div className="App">
+    <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}}
+}
 
 export default App;
