ca397d106f0bc3eb15ae9e1258cccaa88bd083ba
shreeyapatil555 <shreeyadpatil@gmail.com>
2024-04-01 18:00:31 +0530

    auth

--- Frontend/Cashier/src/pages/Home.jsx
+++ Frontend/Cashier/src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import { Navbar, Main, Product, Footer } from "../components";
+// Home.jsx
+
+import React from 'react';
+import { Navbar, Main, Footer } from "../components";
+import { isAuthenticated } from "./auth"; // Import the isAuthenticated function from auth.js
 
 function Home() {
+  // Check if user is authenticated
+  const isLoggedIn = isAuthenticated();
+
   return (
     <>
       <Navbar />
-      <Main />
-      
+      {isLoggedIn ? <Main /> : <p>Please log in to access the main content.</p>}
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
