b241583b3f741a744ed58ffa4b70c34eb3a459e6
shreeyapatil555 <shreeyadpatil@gmail.com>
2024-04-01 11:17:19 +0530

    label

--- Frontend/Cashier/src/pages/Register.jsx
+++ Frontend/Cashier/src/pages/Register.jsx
@@ -25,22 +25,22 @@ const Register = () => {
             },
             body: JSON.stringify(formData)
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            // Add any further actions upon successful registration
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            // Handle any errors here
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Add any further actions upon successful registration
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                // Handle any errors here
+            });
     };
 
     return (
         <div style={{ marginTop: '100px', backgroundColor: 'white', width: '800px', marginLeft: '325px' }}>
             <div className="row my-4 h-100">
                 <h1 className="text-center" style={{ marginTop: '30px' }}>Register</h1>
-                <form style={{ width: '1000px', marginLeft: '40px', marginTop:'30px' }} onSubmit={handleSubmit}>
+                <form style={{ width: '1000px', marginLeft: '40px', marginTop: '30px' }} onSubmit={handleSubmit}>
 
                     <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
                         <div style={{ marginRight: '20px' }}>
@@ -50,7 +50,7 @@ const Register = () => {
                                 className="form-control"
                                 id="Name"
                                 placeholder="Enter Your Name"
-                                style={{width:'300px'}}
+                                style={{ width: '300px' }}
                                 name='full_name'
                             />
                         </div>
@@ -61,13 +61,13 @@ const Register = () => {
                                 className="form-control"
                                 id="Email"
                                 placeholder="name@gmail.com"
-                                style={{width:'300px'}}
+                                style={{ width: '300px' }}
                                 name='email'
                             />
                         </div>
                     </div>
 
-                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop:'20px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop: '20px' }}>
                         <div style={{ marginRight: '20px' }}>
                             <label htmlFor="contact" style={{ marginLeft: '25px' }}>Contact Number</label>
                             <input
@@ -76,24 +76,26 @@ const Register = () => {
                                 id="contactnumber"
                                 name="phone"
                                 placeholder=""
-                                style={{width:'300px'}}
+                                style={{ width: '300px' }}
                                 pattern="[0-9]{10}"
                             />
                         </div>
                         <div>
                             <label htmlFor="role" style={{ marginLeft: '25px' }}>Role</label>
-                            <input
-                                type="text"
+                            <select
                                 className="form-control"
                                 id="role"
-                                placeholder=""
-                                style={{width:'300px'}}
+                                style={{ width: '300px', marginLeft:'30px' }}
                                 name='role'
-                            />
+                            >
+                                <option value="cashier">Cashier</option>
+                                <option value="admin">Admin</option>
+                            </select>
                         </div>
+
                     </div>
 
-                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop:'20px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', marginTop: '20px' }}>
                         <div style={{ marginRight: '20px' }}>
                             <label htmlFor="Email" style={{ marginLeft: '25px' }}>Cashier ID</label>
                             <input
@@ -101,7 +103,7 @@ const Register = () => {
                                 className="form-control"
                                 id="cashierid"
                                 placeholder=""
-                                style={{width:'300px'}}
+                                style={{ width: '300px' }}
                                 name='username'
                             />
                         </div>
@@ -112,7 +114,7 @@ const Register = () => {
                                 className="form-control"
                                 id="cashierpassword"
                                 placeholder="Password"
-                                style={{width:'300px'}}
+                                style={{ width: '300px' }}
                                 name='password'
                             />
                         </div>
@@ -120,10 +122,10 @@ const Register = () => {
 
 
 
-                    <div className="my-3" style={{ marginLeft: '225px' , paddingTop:'20px'}}>
+                    <div className="my-3" style={{ marginLeft: '225px', paddingTop: '20px' }}>
                         <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
                     </div>
-                    <div className="text-center" style={{marginRight:'90px'}}>
+                    <div className="text-center" style={{ marginRight: '90px' }}>
                         <button class="my-2 mx-auto btn btn-dark" type="submit">
                             Register
                         </button>
