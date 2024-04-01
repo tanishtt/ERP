ed93f0524a95db921f3b5ee7235ffcd723a6c944
shreeyapatil555 <shreeyadpatil@gmail.com>
2024-04-01 17:31:11 +0530

    loginhome

--- Frontend/Cashier/src/index.js
+++ Frontend/Cashier/src/index.js
@@ -15,7 +15,7 @@ root.render(
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Login" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/product" element={<Products />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/about" element={<AboutPage />} />
